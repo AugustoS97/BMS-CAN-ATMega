f2cf6d5214ec8e34431204c5069ed2576fd9f218
Augusto Hernández <ahernama@ull.edu.es>
2021-05-09 22:32:42 +0100

    Añadida V1.0 que permite su carga desde platform-io. Separando el código en librerias.

---
+++ BMS_v1.0/lib/BMS/config_BMS.h
@@ -0,0 +1,18 @@
+#ifndef config_BMS_h
+#define config_BMS_h
+
+#include <stdint.h>
+
+#define CFGR0_addr 1
+#define VUV_addr 2
+#define VOV_addr 3
+#define DCTO_addr 4
+#define NCELL_addr 5
+#define N_NTC_addr 6
+#define TSLEEP_addr 7
+#define MAX_DIFF_CELL_addr 8
+#define BALANCING_TYPE_addr 9
+
+const uint8_t TOTAL_IC=1;
+
+#endif
