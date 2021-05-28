08ff56056073a967bf973afa55ebcf28e74e291c
Augusto <ahernama@ull.edu.es>
2021-05-28 19:34:32 +0100

    Modificados los ID del can a los adecuados.

--- BMS_v1.0/include/config.h
+++ BMS_v1.0/include/config.h
@@ -13,7 +13,7 @@
 #define SOC_MSG_ID 0x47
 #define SOH_MSG_ID 0x48
 #define CURRENT_MSG_ID 0x49
-#define TYPE_BALANCING_MSG_ID 0x3F
+#define TYPE_BALANCING_MSG_ID 0x0E
 #define WARNING_ID 0
 #define VUV_MSG_ID 0X02
 #define VOV_MSG_ID 0x03
@@ -26,7 +26,9 @@
 #define ASK_CONFIG_MSG_ID 0x0B
 #define ANSWER_CONFIG_MSG_ID 0x0C
 #define MAX_DIFF_CELL_MSG_ID 0x0D
-
+#define NCELL_PARALLEL_MSG_ID 0x0F
+#define CURRENT_OFFSET_MSG_ID 0x10
+#define ANSWER_CONFIG_MSG_2_ID 0x11
 
 #define PIN_CURRENT_SENSOR A1 //Pin al que se conecta el sensor de corriente
 #define PIN_ANALOG_NTC A0 //Pin para la lectura de las Temperaturas
